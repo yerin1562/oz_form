653e6eec0ca2356f5368e76117a55c55ce3edbb1
SOLBI <thfqlsla35@gmail.com>
2024-11-19 10:48:53 +0900

    Modify answer API

--- app/views/answer.py
+++ app/views/answer.py
@@ -1,59 +1,70 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session 
-from flask.views import MethodView
-from models import Image, Answer, Question, ImageStatus, Option 
-from config import db
+from flask import Blueprint, request, jsonify
+from models import db, Answer, User, Detail_questions
 
-# 블루프린트 정의
-answer_bp = Blueprint('quiz', __name__)
+answer_bp = Blueprint('answer_bp', __name__)
 
-
-from flask import Blueprint, render_template, request, redirect, url_for, session
-from flask.views import MethodView
-from models import Answer, Question
-from config import db
-
-# 블루프린트 정의
-answer_bp = Blueprint('quiz', __name__)
-
-class AnswerView(MethodView):
-    def post(self, detail_question_id):
-        """
-        사용자의 답변을 저장하고, 다음 질문으로 이동
-        """
-        # 사용자가 선택한 답변 ID 가져오기
-        answer_id = request.form.get('answer')  # 선택된 답변 ID
-        user_id = session.get('user_id')  # 로그인한 사용자 ID를 세션에서 가져오기
-
-        # 세션에 현재 질문 ID 저장
-        session['detail_question_id'] = detail_question_id
-        
-        if not answer_id:
-            return "답변이 선택되지 않았습니다.", 400  # 에러 메시지 반환
-
-        # 데이터베이스에 답변 저장
-        new_answer = Answer(user_id=user_id, detail_question_id=detail_question_id)
-        db.session.add(new_answer)
-        db.session.commit()
-
-        # 다음 질문 찾기
-        next_question = Question.query.filter(Question.sqe > detail_question_id).order_by(Question.sqe.asc()).first()
-
-        if next_question:
-            # 다음 질문으로 리디렉션
-            return redirect(url_for('quiz.answer', detail_question_id=next_question.id))
-        else:
-            # 질문이 끝나면 결과 페이지로 이동
-            return redirect(url_for('quiz.result'))
-
-# 블루프린트에 메소드 뷰 등록
-answer_bp.add_url_rule('/answer/<int:detail_question_id>', view_func=AnswerView.as_view('answer'))
-
-# Result 페이지 (모든 질문 완료 후 통계 출력)
-@answer_bp.route('/result', methods=['GET'])
-def result():
+# 1. 응답 저장 (사용자가 설문 응답을 제출할 때)
+@answer_bp.route('/submit_answer', methods=['POST'])
+def submit_answer():
     """
-    결과 페이지를 렌더링
+    사용자가 설문에 응답을 제출하는 라우트
+    요청 본문에서 사용자 ID, 질문 ID 및 응답을 받아 데이터베이스에 저장
     """
-    return render_template('result.html')
+    data = request.get_json()  # 요청 본문에서 JSON 데이터 받기
+    
+    # 필수 데이터가 모두 있는지 확인
+    if not all(key in data for key in ('user_id', 'detail_question_id')):
+        return jsonify({"message": "Missing required fields"}), 400
+    
+    user_id = data['user_id']
+    detail_question_id = data['detail_question_id']
+    
+    # 응답을 데이터베이스에 저장
+    new_answer = Answer(
+        user_id=user_id,
+        detail_question_id=detail_question_id
+    )
+    
+    db.session.add(new_answer)
+    db.session.commit()
+    
+    return jsonify({"message": "Answer submitted successfully", "answer_id": new_answer.id}), 201
+
+
+# 2. 특정 사용자의 응답 조회
+@answer_bp.route('/user_answers/<int:user_id>', methods=['GET'])
+def get_user_answers(user_id):
+    """
+    특정 사용자가 제출한 모든 응답을 조회하는 라우트
+    user_id에 해당하는 사용자가 응답한 모든 질문에 대한 응답을 반환
+    """
+    answers = Answer.query.filter_by(user_id=user_id).all()  # 해당 사용자에 대한 모든 응답 조회
+    
+    # 사용자가 응답한 결과가 없으면
+    if not answers:
+        return jsonify({"message": "No answers found for this user"}), 404
+    
+    # 응답 리스트 반환
+    answers_list = [answer.to_dict() for answer in answers]
+    return jsonify({"answers": answers_list}), 200
+
+
+# 3. 특정 질문에 대한 응답 조회 (여러 응답을 묶어서 반환)
+@answer_bp.route('/question_answers/<int:question_id>', methods=['GET'])
+def get_question_answers(question_id):
+    """
+    특정 질문에 대한 모든 응답을 조회하는 라우트
+    detail_question_id에 해당하는 질문에 대한 모든 응답을 반환
+    """
+    answers = Answer.query.filter_by(detail_question_id=question_id).all()  # 해당 질문에 대한 모든 응답 조회
+    
+    # 질문에 대한 응답이 없으면
+    if not answers:
+        return jsonify({"message": "No answers found for this question"}), 404
+    
+    # 응답 리스트 반환
+    answers_list = [answer.to_dict() for answer in answers]
+    return jsonify({"answers": answers_list}), 200
+
 
 
