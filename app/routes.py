f60d62ba2772792281581986c39a4c28c1184e63
yeringwak <yerin1562@gmail.com>
2024-11-21 22:50:39 +0900

    commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .models import db, User, Question, Detail_questions, Answer, Image
-from sqlalchemy.orm import joinedload
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from app import db
+from app.models import User, Question, Detail_questions, Answer, Image
+import plotly.express as px
+import plotly.io as pio
 
 bp = Blueprint('bp', __name__)
 
@@ -8,83 +10,79 @@ bp = Blueprint('bp', __name__)
 @bp.route('/')
 def index():
     image = Image.query.filter_by(id=5).first()
-
     # 이미지가 있으면 URL을 전달, 없으면 None을 전달
     image_url = image.url if image else None
     
     return render_template('index.html',image_url=image_url)
 
-# 회원가입 후 question 페이지로 이동
+
+# 회원가입 페이지 (signup.html)
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        gender = request.form['gender']
-        mbti = request.form['mbti']
+        data = request.get_json()
 
-        user = User(name=name, age=age, gender=gender, mbti=mbti)
+        # 사용자 정보를 받아서 데이터베이스에 저장
+        user = User(
+            name=data['name'],
+            age=data['age'],
+            gender=data['gender'],
+            mbti=data['mbti']
+        )
+
         db.session.add(user)
         db.session.commit()
 
-        # 사용자가 등록된 후, 첫 번째 질문으로 리디렉션
-        return redirect(url_for('bp.question', question_id=1, user_id=user.id))
-
+        # 성공적으로 가입한 후 user_id를 반환
+        return redirect(f'/question/1?user_id={user.id}')
+    
+    # GET 요청일 경우 signup.html 렌더링
     return render_template('signup.html')
 
-# 질문 페이지, 이미지와 질문 출력
+
+# 질문 페이지 (question.html) - 질문 1부터 4까지 처리
 @bp.route('/question/<int:question_id>', methods=['GET', 'POST'])
 def question(question_id):
-    user_id = request.args.get('user_id')
-    question = Question.query.filter_by(id=question_id).first()
-    image = question.image  # 해당 질문에 대한 이미지
-    detail_questions = Detail_questions.query.filter_by(question_id=question_id).all()
+    user_id = request.args.get('user_id')  # URL에서 user_id 받기
+    question = Question.query.get(question_id)  # 현재 질문
+    image = Image.query.filter_by(id=question.image_id).first()  # 해당 질문에 맞는 이미지
 
+    # POST 요청일 경우 답변을 저장
     if request.method == 'POST':
-        # 사용자가 선택한 답변 저장
-        for dq in detail_questions:
-            answer = request.form.get(str(dq.id))  # answer는 폼 데이터에서 가져옴
-            if answer:
-                user_answer = Answer(user_id=user_id, detail_question_id=dq.id)
-                db.session.add(user_answer)
-
+        # 답변 처리
+        detail_question_ids = request.form.getlist('detail_question_id')
+        for detail_question_id in detail_question_ids:
+            answer = Answer(user_id=user_id, detail_question_id=detail_question_id)
+            db.session.add(answer)
         db.session.commit()
 
-        # 다음 질문으로 리디렉션
-        if question_id < 4:
-            return redirect(url_for('bp.question', question_id=question_id + 1, user_id=user_id))
-        else:
+        # 마지막 질문이면 결과 페이지로 이동
+        if question_id == 4:
             return redirect(url_for('bp.results', user_id=user_id))
+        # 아니면 다음 질문으로 이동
+        return redirect(url_for('bp.question', question_id=question_id + 1, user_id=user_id))
 
-    return render_template('question.html', question=question, image=image, detail_questions=detail_questions)
+    # 해당 질문에 대한 detail_questions 가져오기
+    detail_questions = Detail_questions.query.filter_by(question_id=question.id).all()
 
-# 결과 페이지 - 통계 생성
-@bp.route('/results')
-def results():
-    user_id = request.args.get('user_id')
-    
-    # 사용자의 모든 답변 가져오기
+    return render_template('question.html', question=question, image=image, detail_questions=detail_questions, user_id=user_id)
+
+# 결과 페이지 (result.html) - 답변 통계를 Plotly로 출력
+@bp.route('/results/<int:user_id>', methods=['GET'])
+def results(user_id):
+    # 사용자에 해당하는 답변 가져오기
     answers = Answer.query.filter_by(user_id=user_id).all()
 
-    # 답변 통계 처리 (예: 각 질문에 대한 답변 빈도수 계산)
-    result_data = {}
+    # 답변 통계를 분석하기 위한 데이터 준비
+    answer_data = {}
     for answer in answers:
-        dq = Detail_questions.query.filter_by(id=answer.detail_question_id).first()
-        if dq.question_id not in result_data:
-            result_data[dq.question_id] = {}
-        result_data[dq.question_id][dq.content] = result_data[dq.question_id].get(dq.content, 0) + 1
+        detail_question = Detail_questions.query.get(answer.detail_question_id)
+        if detail_question.id not in answer_data:
+            answer_data[detail_question.id] = 0
+        answer_data[detail_question.id] += 1
 
-    # Plotly 그래프를 위한 데이터 준비
-    import plotly.graph_objects as go
+    # Plotly 그래프 준비
+    fig = px.bar(x=list(answer_data.keys()), y=list(answer_data.values()), labels={'x': 'Detail Question ID', 'y': 'Answer Count'})
+    graph_html = pio.to_html(fig, full_html=False)
 
-    fig = go.Figure()
-
-    for question_id, answers in result_data.items():
-        for content, count in answers.items():
-            fig.add_trace(go.Bar(
-                x=[content], y=[count],
-                name=f'Question {question_id}: {content}'
-            ))
-
-    graph_html = fig.to_html(full_html=False)
     return render_template('results.html', graph_html=graph_html)
