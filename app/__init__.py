77e5986a7032105b58f0ceb827df144b9f1de94a
SOLBI <thfqlsla35@gmail.com>
2024-11-22 17:29:56 +0900

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import api, db
 from flask import Flask
 from flask.cli import with_appcontext
 from flask_migrate import Migrate
-from app.routes import bp as bp  # 블루프린트 임포트
+from app.routes import bp as main_bp  # 블루프린트 임포트
 import app.models
 
 migrate = Migrate()
@@ -29,9 +29,14 @@ def create_app():
     app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
     app.config['OPENAPI_SWAGGER_UI_URL'] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+    db.init_app(app)
+    api.init_app(app)
+
+    migrate.init_app(app, db)
+
 
     # 블루 프린트 등록
-    app.register_blueprint(bp)
+    app.register_blueprint(main_bp)
 
     @click.command("init-db")
     @with_appcontext
